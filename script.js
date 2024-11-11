ce115824144770b1750ac9b9e4401016d19b2a09
Irlan <irlan@mail.com>
2024-11-11 17:59:36 -0300

    adicionando página de reserva

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const nav = document.querySelector('nav');
     if (window.scrollY > 0) {
         nav.classList.add('nav-transparent');
@@ -6,3 +6,41 @@ window.addEventListener('scroll', function() {
         nav.classList.remove('nav-transparent');
     }
 });
+let assentos = document.querySelector(".total-assento");
+for (let i = 0; i < 59; i++) {
+    let randint = Math.floor(Math.random() * 2);
+    let booked = randint === 1 ? "reservado" : "";
+
+    assentos.insertAdjacentHTML(
+        "beforeend",
+        `<input type="checkbox" name="ingresso" id="s${i + 2}" class="assento-checkbox"/>
+         <label for="s${i + 2}" class="assento ${booked}"></label>`
+    );
+}
+
+let precoTotal = document.querySelector(".conta")
+let quantidadeIngresso = document.querySelector(".quantidade")
+let precoIngresso = 16.20
+
+function atualizarPreco() {
+    let ingressosSelecionados = document.querySelectorAll(".assento-checkbox:checked").length;
+    quantidadeIngresso.textContent = ingressosSelecionados;
+    precoTotal.textContent = (ingressosSelecionados * precoIngresso).toFixed(2);
+}
+
+assentos.addEventListener("change", function (e) {
+    if (e.target.classList.contains("assento-checkbox")) {
+        atualizarPreco()
+    }
+});
+atualizarPreco();
+
+
+let reservouAssentos = function() {
+    let ingressosSelecionados = document.querySelectorAll(".assento-checkbox:checked").length;
+    if(ingressosSelecionados >= 2){
+        window.alert("Você reservou os assentos!")
+    } else {
+        window.alert("Você reservou o assento!")
+    }
+}
