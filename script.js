287c8e78e0b3b5f5076b8def56d16eca2c6bb569
Davi <daviialcantara1@gmail.com>
2024-11-11 18:14:10 -0300

    add  cards-swiper

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ window.addEventListener('scroll', function() {
         nav.classList.remove('nav-transparent');
     }
 });
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 4,
+      centeredSlides: true,
+      spaceBetween: 50,
+      pagination: {
+      
+        type: "fraction",
+      },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
